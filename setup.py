33c9c305fe7226124072f1ac0c06e73f524fa939
Elia Wäfler <158746986+eliawaefler@users.noreply.github.com>
2024-05-28 13:25:39 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,14 @@ setup(
     name='MyUtils',
     version='0.1.0',  # Update this version number as needed
     packages=find_packages(),
+    install_requires=[
+        'setuptools~=57.4.0',
+        'requests~=2.31.0',
+        'openai~=1.23.6',
+        'ifcopenshell~=0.7.0.231218',
+        'neo4j~=5.20.0',
+        'numpy~=1.26.4'
+    ],
     description='A collection of utility functions for multiple projects',
     author='Elia Wäfler',
     author_email='elia.waefler@gmail.com',
